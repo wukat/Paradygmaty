6aefcf4bf8e981594cc0549dd7b75af60270cbcf
wukat <wojciechksprk@gmail.com>
2014-03-05 12:16:18 +0100

    first labs

---
+++ Prolog/fam2.pl
@@ -0,0 +1,70 @@
+rodzic(kasia,robert).
+rodzic(tomek,robert).
+rodzic(tomek,eliza).
+rodzic(robert,anna).
+rodzic(robert,magda).
+rodzic(magda,jan).
+
+kobieta(kasia).
+kobieta(eliza).
+kobieta(magda).
+kobieta(anna).
+
+mezczyzna(tomek).
+mezczyzna(robert).
+mezczyzna(jan).
+
+
+famme(kasia).
+homme(krzys).
+parent(kasia,krzys).
+
+
+matka(X,Y) :-
+	rodzic(X,Y),
+	kobieta(X).
+
+ojciec(X,Y) :-
+	rodzic(X,Y),
+	mezczyzna(X).
+
+brat(X,Y) :-
+	mezczyzna(X),
+	rodzic(Z,X),
+	rodzic(Z,Y),
+	X \= Y.
+
+siostra(X,Y) :-
+	kobieta(X),
+	rodzic(Z,X),
+	rodzic(Z,Y),
+	X \= Y.
+
+babcia(X,Y) :-
+	matka(X,Z),
+	rodzic(Z,Y).
+
+dziadek(X,Y) :-
+	ojciec(X,Z),
+	rodzic(Z,Y).
+
+przodek(X,Y) :-
+	rodzic(X,Y).
+
+przodek(X,Z) :-
+	rodzic(X,Y),
+	przodek(Y,Z).
+
+potomek(X,Y) :-
+	rodzic(Y,X).
+
+potomek(X,Z) :-
+	rodzic(Z,Y),
+	potomek(X,Y).
+
+krewny(X,Y) :-
+	przodek(Z,Y),
+	przodek(Z,X),
+	X\=Y.
+
+
