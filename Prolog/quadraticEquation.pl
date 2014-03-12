155569cbbabcdc4258165f076741a1dbcf9db85f
wukat <wojciechksprk@gmail.com>
2014-03-12 11:20:52 +0100

    quadratic fixed

--- Prolog/quadraticEquation.pl
+++ Prolog/quadraticEquation.pl
@@ -6,12 +6,12 @@ kwadrat(A, B, C, Wynik) :-
 	rozwiaz(A,B,Delta,Wynik,X).
 
 rozwiaz(A,B,0,Wynik,0) :-
-	Wynik is -B / 2*A.
+	Wynik is -B / (2*A).
 
 rozwiaz(A,B,Delta,Wynik,0) :-
 	Delta > 0,
-	Wynik is (-B - sqrt(Delta)) / 2*A.
+	Wynik is (-B - sqrt(Delta)) / (2*A).
 
 rozwiaz(A,B,Delta,Wynik,1) :-
 	Delta > 0,
-	Wynik is (B - sqrt(Delta)) / 2*A.
+	Wynik is (B - sqrt(Delta)) / (2*A).
