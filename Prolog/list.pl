381c10653282c5f42b6ad4f4fec075d493c38805
wukat <wojciechksprk@gmail.com>
2014-03-12 23:26:13 +0100

    some tricky excercises

--- Prolog/list.pl
+++ Prolog/list.pl
@@ -1,13 +1,41 @@
-porownaj(List) :-
-    [A,B,C,D|Rest] = List,
+porownaj(Lista) :-
+    [_,_,C,D|_] = Lista,
     C = D,
     write('Tak').
 
-porownaj(List) :-
-    [A,B,C,D|Rest] = List,
+porownaj(Lista) :-
+    [_,_,C,D|_] = Lista,
     C \= D,
     write('Nie').
 
-zamien(List, X) :-
-	[A, B, C, D|Rest] = List,
-	append([A,B,D,C], Rest, X).
+zamien(Lista, X) :-
+	[A, B, C, D|Reszta] = Lista,
+	append([A,B,D,C], Reszta, X).
+
+nalezy(X,[X|_]).
+nalezy(X,[_|Yogon]) :-
+	nalezy(X,Yogon).
+
+dlugosc([],0).
+dlugosc([_|Ogon],Dlug) :-
+	dlugosc(Ogon,X),
+	Dlug is X+1.
+
+a2b([],[]).
+a2b([a|Ta],[b|Tb]) :- 
+   a2b(Ta,Tb).
+
+sklej([],X,X).
+sklej([X|L1],L2,[X|L3]) :-
+	sklej(L1,L2,L3).
+
+nalezy1(X,L) :-
+	sklej(_,[X|_],L).
+
+ostatni1(Element,Lista) :- %z uzyciem sklej
+	sklej(_,[Element], Lista).
+
+ostatni(Element, [Element]).
+ostatni(Element, Lista) :- %bez sklej
+	[_|Reszta] = Lista,
+	ostatni(Element,Reszta).
